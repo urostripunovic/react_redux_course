4e0de56ac0174eeed0eb665e925c2ceb40c73a7d
urostripunovic <uros_1997@hotmail.com>
2022-07-05 16:07:18 +0200

    Assigment userSlice API implemented

--- 07_lesson/src/features/users/usersSlice.js
+++ 07_lesson/src/features/users/usersSlice.js
@@ -1,29 +1,38 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../api/apiSlice";
 
-const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+const usersAdapter = createEntityAdapter();
 
-const initialState = []
+const initialState = usersAdapter.getInitialState()
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-    const response = await axios.get(USERS_URL);
-    return response.data
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getUsers: builder.query({
+            query: () => '/users',
+            transformResponse: responseData => {
+                return usersAdapter.setAll(initialState, responseData)
+            },
+            providesTags: (result, error, arg) => [
+                { type: 'User', id: "LIST" },
+                ...result.ids.map(id => ({ type: 'User', id }))
+            ]
+        }),
+    })
 })
 
-const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {},
-    extraReducers(builder) {
-        builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            return action.payload;
-        })
-    }
-})
+export const {
+    useGetUsersQuery,
+} = usersApiSlice
 
-export const selectAllUsers = (state) => state.users;
+export const selectUserResult = usersApiSlice.endpoints.getUsers.select()
 
-export const selectUserById = (state, userId) =>
-    state.users.find(user => user.id === userId)
+const selectUsersData = createSelector(
+    selectUserResult,
+    usersResult => usersResult.data
+)
 
-export default usersSlice.reducer
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById,
+    selectIds: selectUserIds
+} = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState)
--- 07_lesson/src/index.js
+++ 07_lesson/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import { fetchPosts } from './features/posts/postsSlice';
-import { fetchUsers } from './features/users/usersSlice';
+import { postsApiSlice } from './features/posts/postsSlice';
+import { usersApiSlice } from './features/users/usersSlice';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
+store.dispatch(postsApiSlice.endpoints.getPosts.initiate());
+store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
 
 ReactDOM.render(
   <React.StrictMode>
